cda42b0c257b3b6954036391ee0bc0ef3c84050b
Dawid Dahl <dawiddahl@gmail.com>
2020-04-03 21:13:05 +0200

    removed refresh route

--- server/api/routes/api.ts
+++ server/api/routes/api.ts
@@ -2,13 +2,11 @@ import express from "express";
 import registerRouter from "./register";
 import loginRouter from "./login";
 import protectedRouter from "./protected";
-import refreshRouter from "./refresh";
 
 const apiRouter = express.Router();
 
 apiRouter.use("/register", registerRouter);
 apiRouter.use("/login", loginRouter);
-apiRouter.use("/refresh", refreshRouter);
 apiRouter.use("/protected", protectedRouter);
 
 export default apiRouter;
